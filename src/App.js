afdd3c7d81b4081f3ee32743d366884fd6a83ed9
AnnaHort <annahort3d@gmail.com>
2023-12-06 16:00:13 +0200

    adding BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { GlobalStyle } from "./GlobalStyle";
+
+import { lazy, Suspense } from "react";
+
+const MyComponent = lazy(() =>
+  import("./components/MyComponent/MyComponent.jsx")
+);
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MyComponent />} />
+        </Routes>
+      </Suspense>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/MyComponent/MyComponent.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MyComponent = () => {
+  return <h1>How are you?</h1>;
+};
+export default MyComponent;
