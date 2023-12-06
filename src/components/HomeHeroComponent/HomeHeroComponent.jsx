4579fe0025d03c3c091aa4d36c311beb811823af
AnnaHort <annahort3d@gmail.com>
2023-12-07 00:34:15 +0200

    added styled HomeHeroComponent

--- src/components/HomeHeroComponent/HomeHeroComponent.jsx
+++ src/components/HomeHeroComponent/HomeHeroComponent.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { HomeHeroContainer } from "./HomeHeroComponent.styled";
+import {
+  HomeHeroContainer,
+  ListElementStyled,
+  ListStyle,
+  ListTextStyle,
+  TitleStyle,
+} from "./HomeHeroComponent.styled";
 import { MdOutlineDoneOutline } from "react-icons/md";
 
 const HomeHeroComponent = () => {
   return (
     <HomeHeroContainer>
-      <h2>Прокат автомобілів по всій Україні</h2>
-      <ul>
-        <li>
-          <MdOutlineDoneOutline /> Безкоштовне скасування для більшості номерів
-        </li>
-        <li>
-          <MdOutlineDoneOutline /> Понад 60 000 пунктів прокату
-        </li>
-        <li>
-          <MdOutlineDoneOutline />
-          Служба підтримки надає допомогу понад 40 мовами
-        </li>
-      </ul>
+      <TitleStyle>Прокат автомобілів по всій Україні</TitleStyle>
+      <ListStyle>
+        <ListElementStyled>
+          <MdOutlineDoneOutline size={40} color="#fff"/>
+          <ListTextStyle>
+            Безкоштовне скасування для більшості номерів
+          </ListTextStyle>
+        </ListElementStyled>
+        <ListElementStyled>
+          <MdOutlineDoneOutline size={40} color="#fff"/>
+          <ListTextStyle>Понад 60 000 пунктів прокату</ListTextStyle>
+        </ListElementStyled>
+        <ListElementStyled>
+          <MdOutlineDoneOutline size={40} color="#fff"/>
+          <ListTextStyle>
+            Служба підтримки надає допомогу понад 40 мовами
+          </ListTextStyle>
+        </ListElementStyled>
+      </ListStyle>
     </HomeHeroContainer>
   );
 };
--- src/components/HomeHeroComponent/HomeHeroComponent.styled.js
+++ src/components/HomeHeroComponent/HomeHeroComponent.styled.js
@@ -3,7 +3,35 @@ import styled from "styled-components";
 export const HomeHeroContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   gap: 24px;
-  margin-bottom: 40px;
+  margin-bottom: 60px;
+  padding: 40px;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 128, 128, 0.5),
+      rgba(0, 128, 128, 0)
+    ),
+    url("/pictures/HomeHeroBg/pexels-mike-bird-170286.jpg");
+  background-size: cover;
+  background-position: center;
+`;
+
+export const TitleStyle = styled.h2`
+  font-size: 56px;
+  line-height: 1.25;
+  color: #fff;
+  text-align: center;
+  text-shadow: 1px -3px 6px rgba(0,0,0,0.83);
+`;
+
+export const ListStyle = styled.ul``;
+export const ListElementStyled = styled.li`
+  display: flex;
+  gap: 8px;
+`;
+export const ListTextStyle = styled.p`
+  color: #fff;
+  font-size: 24px;
+  line-height: 1.25;
+  text-shadow: 1px -3px 6px rgba(0,0,0,0.83);
 `;
