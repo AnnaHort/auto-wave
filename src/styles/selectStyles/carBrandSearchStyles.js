22300266ea484bac1f3352c8f58b45805e5e44ba
AnnaHort <annahort3d@gmail.com>
2023-12-07 15:49:19 +0200

    added folder styles
    -added selectStyles folder
    -added props for select styles

---
+++ src/styles/selectStyles/carBrandSearchStyles.js
@@ -0,0 +1,26 @@
+
+export const carBrandSearchStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      borderColor: "#d9d9d9",
+      boxShadow: state.isFocused ? "transparent" : "none",
+      width: "224px",
+      height: "48px",
+      "&:hover": {
+        borderColor: "#d9d9d9",
+      },
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: "transparent",
+      color: state.isFocused ? "#121417" : "rgba(18, 20, 23, 0.20)",
+      "&:hover": {
+        backgroundColor: state.isFocused ? "transparent" : "#transparent",
+      },
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      width: "224px",
+    }),
+  };
+  
