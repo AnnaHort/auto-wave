2c3aa2cffb20e1cefcb9d5783418a9a8e60798b6
AnnaHort <annahort3d@gmail.com>
2023-12-08 22:25:41 +0200

    Updates

---
+++ static/js/678.dec54dab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkauto_wave=self.webpackChunkauto_wave||[]).push([[678],{678:(e,a,s)=>{s.r(a),s.d(a,{default:()=>u});s(791);var t=s(184);const u=()=>(0,t.jsx)("h2",{children:"Favorites"})}}]);
+//# sourceMappingURL=678.dec54dab.chunk.js.map
