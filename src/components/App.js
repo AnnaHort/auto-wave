54c664cc1dafb0431feaccebf7b00a88a50aaf29
AnnaHort <annahort3d@gmail.com>
2023-12-11 22:45:31 +0200

    modified App.js & index.js~

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
-import { lazy } from 'react';
 import NotFound from './NotFound/NotFound.jsx';
 
 const HeaderList = lazy(() => import('./HeaderList/HeaderList.jsx'));
@@ -13,14 +12,18 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<HeaderList/>}>
-          <Route index element={<Home/>} />
-          <Route path="/catalog" element={<Catalog/>} />
-          <Route path="/favorites" element={<Favorites/>} />
-        </Route>
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+
+          <Routes>
+            <Route path="/" element={<HeaderList />}>
+              <Route index element={<Home />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+  
+      </Suspense>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import App from './components/App';
 
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { HashRouter } from 'react-router-dom';
+import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/auto-wave" future={{ v7_startTransition: true }}>
-
+    {/* <HashRouter basename="/auto-wave" future={{ v7_startTransition: true }}> */}
+    <BrowserRouter basename="/auto-wave">
       <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
       </Provider>
- 
-    </HashRouter>
+    </BrowserRouter>
+    {/*  
+    </HashRouter> */}
   </React.StrictMode>
 );
