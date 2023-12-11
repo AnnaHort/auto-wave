7c8853c17415ef694d567baeb81193fd25cc59ba
AnnaHort <annahort3d@gmail.com>
2023-12-11 22:22:17 +0200

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-// import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter basename="/auto-wave" future={{ v7_startTransition: true }}>
-      {/* <BrowserRouter basename="/auto-wave"> */}
+
       <Provider store={store}>
         <App />
       </Provider>
-      {/* </BrowserRouter> */}
+ 
     </HashRouter>
   </React.StrictMode>
 );
