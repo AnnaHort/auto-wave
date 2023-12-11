42df5d91a639442012fd7ff30d8894c556b27e05
AnnaHort <annahort3d@gmail.com>
2023-12-11 19:04:35 +0200

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from "./redux/store";
+import { store } from './redux/store';
+import { HashRouter } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- 
-     <BrowserRouter basename="/auto-wave">
-     <Provider store={store}>
-      <App />
+    <HashRouter basename="/auto-wave" future={{ v7_startTransition: true }}>
+      {/* <BrowserRouter basename="/auto-wave"> */}
+      <Provider store={store}>
+        <App />
       </Provider>
-    </BrowserRouter>
-   
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
