563c8b592ced094f5d7f9c4c75930931a79f4e9d
AnnaHort <annahort3d@gmail.com>
2023-12-11 18:17:41 +0200

    test debug router error

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
+ 
      <BrowserRouter basename="/auto-wave">
+     <Provider store={store}>
       <App />
+      </Provider>
     </BrowserRouter>
-     </Provider>
+   
   </React.StrictMode>
 );
