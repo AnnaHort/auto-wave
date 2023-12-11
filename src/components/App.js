d00f92197589cbe33a5ecaf5d35167c8ced01481
AnnaHort <annahort3d@gmail.com>
2023-12-11 18:54:20 +0200

    debug GlobalStyle components

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,15 @@ const Favorites = lazy(() => import('../pages/FavoritesPage/Favorites.jsx'));
 function App() {
   return (
     <>
-
-        <Routes>
-          <Route path="/" element={<HeaderList />}>
-            <Route index element={<Home />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
       <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HeaderList/>}>
+          <Route index element={<Home/>} />
+          <Route path="/catalog" element={<Catalog/>} />
+          <Route path="/favorites" element={<Favorites/>} />
+        </Route>
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </>
   );
 }
