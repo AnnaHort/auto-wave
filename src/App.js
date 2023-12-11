641ecfb750c53f4b995be44b191d9e401e403bfc
AnnaHort <annahort3d@gmail.com>
2023-12-11 23:51:13 +0200

    moving APP.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { GlobalStyle } from './GlobalStyle';
-import NotFound from './NotFound/NotFound.jsx';
+import { GlobalStyle } from './components/GlobalStyle.js';
+import NotFound from './components/NotFound/NotFound.jsx';
 
-const HeaderList = lazy(() => import('./HeaderList/HeaderList.jsx'));
-const Home = lazy(() => import('../pages/HomePage/Home.jsx'));
-const Catalog = lazy(() => import('../pages/CatalogPage/Catalog.jsx'));
-const Favorites = lazy(() => import('../pages/FavoritesPage/Favorites.jsx'));
+const HeaderList = lazy(() => import('./components/HeaderList/HeaderList.jsx'));
+const Home = lazy(() => import('./pages/HomePage/Home.jsx'));
+const Catalog = lazy(() => import('./pages/CatalogPage/Catalog.jsx'));
+const Favorites = lazy(() => import('./pages/FavoritesPage/Favorites.jsx'));
 
 function App() {
   return (
