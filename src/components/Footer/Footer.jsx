ee544b7c7c9ecbc27f385ea8be95250bc57c9223
AnnaHort <annahort3d@gmail.com>
2024-02-05 18:33:34 +0200

    deleted unimp;ortant imports in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
-import { FaFacebookF } from 'react-icons/fa';
-import { FaYoutube } from 'react-icons/fa';
 import {
   FooterContainer,
   FooterContainerSocialMedia,
